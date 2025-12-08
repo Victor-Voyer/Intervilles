7f68cb1dc1dc9990300cf9dafbcf15b4a0b51bd3
Victor-Voyer <victor.voyer@oclock.school>
2025-12-08 14:27:42 +0100

    migration for users

---
+++ back_end/src/migrations/20251208130221-create-users.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        unique: true,
+      },
+      first_name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      last_name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+      },
+      validated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      verified_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      promo_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'promos',
+          key: 'id',
+        },
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'roles',
+          key: 'id',
+        },
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
