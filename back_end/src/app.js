0b5ce125eb34279ff8871bb3bf4d01c9e8226b5f
Victor-Voyer <victor.voyer@oclock.school>
2025-12-08 11:10:37 +0100

    initailization back-end, dependances, all folders,  server.js and app.js

---
+++ back_end/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/intervilles', (req, res) => {
+    res.send('Hello World');
+});
+
+
+export default app;
