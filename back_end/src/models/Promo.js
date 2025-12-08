f5b6bb2d78d9320ac697eb1e84a238224ee22d58
Victor-Voyer <victor.voyer@oclock.school>
2025-12-08 13:17:29 +0100

    create promo model

---
+++ back_end/src/models/Promo.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import { Model } from 'sequelize';
+export default (sequelize, DataTypes) => {
+  class Promo extends Model {
+    static associate(models) {
+      Promo.hasMany(models.User, {
+        foreignKey: 'promo_id',
+        as: 'users',
+      });
+    }
+  }
+  Promo.init(
+    {
+        name: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        year: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 2025,
+            },
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Promo',
+        tableName: 'promos',
+        underscored: true,
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    }
+  );
+  return Promo;
+}
